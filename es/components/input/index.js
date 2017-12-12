010e61ffc07e7e6d3ebf61abe64f65ec94436666
wuxiaokang <sinclairwxk@sina.com>
2017-12-12 17:15:08 +0800

    commit

---
+++ es/components/input/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class Input extends React.Component{
+  render(){
+    <input {...this.props} />
+  }
+}
+export default Input
--- index.js
+++ index.js
@@ -4,22 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _components = require('./components');
-
-Object.defineProperty(exports, 'components', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_components).default;
-  }
-});
-
-var _utils = require('./utils');
-
-Object.defineProperty(exports, 'utils', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_utils).default;
-  }
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _components = require('./lib/components');
+var _utils = require('./lib/utils');
+module.exports={
+  components:_components,
+  utils:_utils
+}
